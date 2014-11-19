158b4069816e3984d775bbeb76a12bb45e451a15
tophat8855 <hrfarley@gmail.com>
2014-11-18 16:23:40 -0800

    Create the terms page and link it in the footer

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 root 'welcome#index'
 
 get '/about' => 'pages#about'
+get '/terms' => 'pages#terms'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
