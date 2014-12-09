31ef96c4b9c6cf6ab543a07020cea13dbe050d66
tophat8855 <hrfarley@gmail.com>
2014-12-09 15:23:31 -0800

    users cannot enter a date in the past when creating a task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,7 @@
 class Task < ActiveRecord::Base
   validates :description, presence: true
+  validate :due_date_plausible
+
   def index
     @tasks = Task.all
     respond_to do |format|
@@ -8,6 +10,12 @@ class Task < ActiveRecord::Base
         headers['Content-Disposition'] = "attachment; filename=\"task-list\""
         headers['Content-Type'] ||= 'text/csv'
       end
+    end
+  end
+
+  def due_date_plausible
+    if due_date.present? && due_date < Date.today
+      errors.add(:due_date, "must be today or later")
     end
   end
 end
