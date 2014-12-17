43186cd7eafc9827f4ff400c679e0a1e245f2922
tophat8855 <hrfarley@gmail.com>
2014-12-17 15:53:47 -0800

    add password to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208221916) do
+ActiveRecord::Schema.define(version: 20141217212504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20141208221916) do
     t.string   "email",      null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password"
   end
 
 end
