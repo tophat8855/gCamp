6cadf12baa2c459f7aa75132544bb879f08b912b
tophat8855 <hrfarley@gmail.com>
2014-12-17 20:50:46 -0800

    Users can sign in with valid credentials

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,8 @@ get '/tasks' => 'pages#tasks'
 get '/users' => 'users#index'
 get '/sign-up' => 'registrations#new', as: :signup
 post '/sign-up' => 'registrations#create'
-# get '/sign-in' => 'authentication#new', as: :signin
-# post '/sign-in' => 'authentication#create'
+get '/sign-in' => 'authentication#new', as: :signin
+post '/sign-in' => 'authentication#create'
 get '/sign-out' => 'authentication#destroy', as: :signout
 
 resources :users
