714fb753e9353c11d082552f9a1fd9a9439262a6
tophat8855 <hrfarley@gmail.com>
2014-12-17 20:11:13 -0800

    user password confirmation, email required, uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :first_name, :last_name, :email, presence: true
-  validates :email, uniqueness: true
+  #validates :first_name, :last_name,
+  validates :email, presence: true, uniqueness: true, confirmation: true
   has_secure_password
 end
