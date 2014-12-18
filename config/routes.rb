250cb1adaedd0a2e8d14e1b588d334a90de248d4
tophat8855 <hrfarley@gmail.com>
2014-12-17 19:51:30 -0800

    finish authentication stuff

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ get '/sign-up' => 'registrations#new', as: :signup
 post '/sign-up' => 'registrations#create'
 # get '/sign-in' => 'authentication#new', as: :signin
 # post '/sign-in' => 'authentication#create'
-# get '/sign-out' => 'authentication#destroy', as: :signout
+get '/sign-out' => 'authentication#destroy', as: :signout
 
 resources :users
 resources :projects
