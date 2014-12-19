b90ff5742537d1e7c748a894d16141e7e1c3acb7
tophat8855 <hrfarley@gmail.com>
2014-12-19 14:47:30 -0800

    ran migration for new memberships table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141218224427) do
+ActiveRecord::Schema.define(version: 20141219223529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20141218224427) do
   create_table "faqs", force: true do |t|
     t.text     "question"
     t.text     "answer"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "memberships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.string   "role"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
